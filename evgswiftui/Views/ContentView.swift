0383bf61dd930273dba72a496c2c3d83e26c45da
Rafal Pawelec <20691432+ithinksodigital@users.noreply.github.com>
2025-02-16 21:55:38 +0100

    sf symbols change

--- evgswiftui/Views/ContentView.swift
+++ evgswiftui/Views/ContentView.swift
@@ -5,19 +5,17 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack(spacing: 20) {
-      
                 NavigationLink(destination: ItemView()) {
                     HStack {
                         Text("Go to Item")
                         Image(systemName: "arrow.right")
-
                     }
-                        .font(.title2)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                    .font(.title2)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
                 }
                 .padding(.horizontal)
 
@@ -25,47 +23,53 @@ struct ContentView: View {
                     HStack {
                         Text("Go to Demo Screen")
                         Image(systemName: "arrow.right")
-
                     }
                     .font(.title2)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
                 }
                 .padding(.horizontal)
-                
-    
-                
+
                 NavigationLink(destination: CampaignView()) {
                     HStack {
                         Text("Go to Campaign View")
                         Image(systemName: "arrow.right")
-
                     }
-                        .font(.title2)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                    .font(.title2)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
                 }
                 .padding(.horizontal)
+                
+     
+
                 Spacer()
+                
                 VStack {
-                            Text("App created by Rafał Pawelec")
-                                .font(.footnote)
-                                .padding(.horizontal)
-                            
-                            Link("GitHub: @ithinksodigital", destination: URL(string: "https://github.com/ithinksodigital/evg-swiftui")!)
-                                .font(.footnote)
-                                .foregroundColor(.blue)
-                                .padding(.top, 5)
-                        }
-                        .padding(.bottom, 20)
+                    HStack(spacing: 10) {
+                        Image(systemName: "bolt.fill")
+                        Text("App created by Rafał Pawelec")
+                            .font(.footnote)
+                          
+                    }
+                    .padding(.horizontal)
+                    
+                    
+                    
+                    Link("GitHub: @ithinksodigital", destination: URL(string: "https://github.com/ithinksodigital/evg-swiftui")!)
+                        .font(.footnote)
+                        .foregroundColor(.blue)
+                        .padding(.top, 5)
+                }
+                .padding(.bottom, 20)
             }
-            .onAppear() {
+            .onAppear {
                 Evergage.sharedInstance().globalContext?.trackAction("Viewed Content Screen")
             }
             .navigationTitle("SwiftUI + Evergage")
