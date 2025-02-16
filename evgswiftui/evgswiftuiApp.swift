28f219d60b17ea7e31fca9dd92976fd77e45aad2
Rafal Pawelec <20691432+ithinksodigital@users.noreply.github.com>
2025-02-16 21:05:02 +0100

    Initial Commit

---
+++ evgswiftui/evgswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  evgswiftuiApp.swift
+//  evgswiftui
+//
+//  Created by Rafał Pawelec on 16/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct evgswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
